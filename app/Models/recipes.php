1f612125829de1efb1d604aed84735e5ba42c106
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-07-27 12:41:23 -0400

    creacion de modelos y features

---
+++ app/Models/recipes.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class recipes extends Model
+{
+    use HasFactory;
+    
+    protected $fiallable =[
+        'name',
+        'instructions',
+        'glass_type',
+        'garnish',
+        'image_url',
+        'user_id',
+        'source',
+        'is_private',
+        'source_api_id',
+    ]
+
+}
