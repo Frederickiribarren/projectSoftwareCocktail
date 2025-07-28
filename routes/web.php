55bf0d4188a3a7de1f452b6d8ba065f59e3a825e
Frederick Iribarren <frederickiribarren@gmail.com>
2025-07-28 16:10:27 -0400

    se incorpora login

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pageMain');
 });
+
+Route::get('/login', function () {
+    return view('components.login');
+});
