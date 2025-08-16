9743e3e82bde6ea98125c37a9dfd4c77eed68a26
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-08-16 14:40:48 -0400

    crud user_favorites sin vista

--- app/Http/Controllers/user_favoritesController.php
+++ app/Http/Controllers/user_favoritesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\user_favorites;
 
 class user_favoritesController extends Controller
 {
@@ -10,5 +11,46 @@ class user_favoritesController extends Controller
     {
         $user_favorites = user_favorites::all();
         return view('user_favorites.index', compact('user_favorites'));
+    }
+    public function create()
+    {
+        return view('user_favorites.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'user_id' => 'required',
+            'recipe_id' => 'required'
+        ]);
+        
+        return redirect()->route('user_favorites.index');
+    }
+
+    public function show(user_favorites $user_favorite)
+    {
+        return view('user_favorites.show', compact('user_favorite'));
+    }
+
+    public function edit(user_favorites $user_favorite)
+    {
+        return view('user_favorites.edit', compact('user_favorite')); 
+    }
+
+    public function update(Request $request, user_favorites $user_favorite)
+    {
+        $validated = $request->validate([
+            'user_id' => 'required',
+            'cocktail_id' => 'required'
+        ]);
+        
+        $user_favorite->update($validated);
+        return redirect()->route('user_favorites.index');
+    }
+
+    public function destroy(user_favorites $user_favorite)
+    {
+        $user_favorite->delete();
+        return redirect()->route('user_favorites.index');
     }
 }
