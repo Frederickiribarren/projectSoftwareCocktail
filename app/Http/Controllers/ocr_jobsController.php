5e71160857dedeec8260cdf32a72b30bacd255ab
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-08-16 12:21:11 -0400

    creacion de CRUD sin vista de inventories

--- app/Http/Controllers/ocr_jobsController.php
+++ app/Http/Controllers/ocr_jobsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use APP\Models\ocr_jobs;
 
 class ocr_jobsController extends Controller
 {
@@ -11,4 +12,5 @@ class ocr_jobsController extends Controller
         $ocr_jobs = ocr_jobs::all();
         return view('ocr_jobs.index', compact('ocr_jobs'));
     }
+
 }
