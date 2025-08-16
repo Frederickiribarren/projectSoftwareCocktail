b834d7fb771594b30d7cecbfc23921442f849dd4
ALEJANDRO GAMBIN <ale_gambin@hotmail.com>
2025-08-16 12:24:42 -0400

    creacion de crud sin vista

--- app/Http/Controllers/ocr_jobsController.php
+++ app/Http/Controllers/ocr_jobsController.php
@@ -12,5 +12,56 @@ class ocr_jobsController extends Controller
         $ocr_jobs = ocr_jobs::all();
         return view('ocr_jobs.index', compact('ocr_jobs'));
     }
+    public function create()
+    {
+        return view('ocr_jobs.create');
+    }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        ocr_jobs::create($request->all());
+
+        return redirect()->route('ocr_jobs.index')
+                         ->with('success', 'OCR Job created successfully.');
+    }
+
+    public function show($id)
+    {
+        $ocr_job = ocr_jobs::findOrFail($id);
+        return view('ocr_jobs.show', compact('ocr_job'));
+    }
+
+    public function edit($id)
+    {
+        $ocr_job = ocr_jobs::findOrFail($id);
+        return view('ocr_jobs.edit', compact('ocr_job'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        $ocr_job = ocr_jobs::findOrFail($id);
+        $ocr_job->update($request->all());
+
+        return redirect()->route('ocr_jobs.index')
+                         ->with('success', 'OCR Job updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $ocr_job = ocr_jobs::findOrFail($id);
+        $ocr_job->delete();
+
+        return redirect()->route('ocr_jobs.index')
+                         ->with('success', 'OCR Job deleted successfully.');
+    }
 }
